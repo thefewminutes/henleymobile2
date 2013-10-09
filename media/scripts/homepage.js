e7574df791a11643d77f3e8d39eedfec2315c6a4
Mark Johnson <mark@produkt.us>
2013-10-09 15:54:41 -0700

    add search close btn
    add js and css to remove search icon and replace with close tab. add js
    to re-add search icon after search box closes

--- media/scripts/homepage.js
+++ media/scripts/homepage.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
 	
-	$("#btn_search").click(function() {
-		$('#searchpanel').slideToggle("fast");
+	$('#btn_search').click(function() {
+		$('#searchpanel').slideDown("fast");
+		$('#searchTrigger').toggle();
+		$('#searchCloser').toggle();
+	});
+	
+	$('#searchCloser').click(function() {
+		$('#searchpanel').slideUp("fast");
+		$('#searchTrigger').toggle();
+		$('#searchCloser').toggle();
 	});
 	
 });
