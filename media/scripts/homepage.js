781325966383505ba6a161213059514d110981ac
Mark Johnson <mark@produkt.us>
2013-10-14 14:00:39 -0700

    format bottom nav
    add slideup that actually works

--- media/scripts/homepage.js
+++ media/scripts/homepage.js
@@ -1,22 +1,20 @@
 $(document).ready(function() {
 	
 	$('#btn_search').click(function() {
-		$('#searchpanel').slideDown("fast");
+		$('#searchpanel').slideDown("slow");
 		$('#searchTrigger').toggle();
 		$('#searchCloser').toggle();
 	});
 	
 	$('#searchCloser').click(function() {
-		$('#searchpanel').slideUp("fast");
+		$('#searchpanel').slideUp("slow");
 		$('#searchTrigger').toggle();
 		$('#searchCloser').toggle();
 	});
 	
-		$('#navOpen').click(function() {
-			$('#bottomNav').slideDown();
-	//	$('#bottomNav').show();
-		//$('#searchTrigger').toggle();
-		//$('#searchCloser').toggle();
-	});
+	$("#navOpen").click(function(){
+        $("#bottomNav").slideToggle("slow");
+  		$('#navArrow').toggleClass('arrow-up-rel').toggleClass('arrow-down-rel');
+    });
 	
 });
